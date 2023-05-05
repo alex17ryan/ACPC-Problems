84e291e52b431d701abc3ec909b90ba5658da8e5
Alex Ryan <115376787+alex17ryan@users.noreply.github.com>
2023-05-05 15:30:43 +0100

    Add files via upload

---
+++ theWalkingAdam.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int countSteps(char code[]) {
+
+    int s = 0;
+
+    for (int i = 0; i < (int)strlen(code); i++) {
+        if(code[i] == 'U') {
+            s++;
+        }
+        else if (s != 0 && code[i] == 'D'){
+            return s;
+        }
+    }
+
+    return s;
+}
+
+int main() {
+
+    FILE *input_file, *output_file;
+
+    int T, steps[100];
+    char code[100];
+
+    fscanf(input_file, "%d", &T);
+
+    for(int i = 0; i < T; i++) {
+        fscanf(input_file, "%s", code);
+        steps[i] = countSteps(code);
+        fprintf(output_file, "%i\n", steps[i]);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
+    return 0;
+}
